69bb8ace4f40507eddf3150f4bdd862c7b9f16a8
HeoEunJae <dmswo159@gmail.com>
2022-08-26 19:31:25 +0900

    로그인및회원가입

--- src/main/java/com/insta/project/MainController.java
+++ src/main/java/com/insta/project/MainController.java
@@ -1,15 +1,26 @@
 package com.insta.project;
 
+import com.insta.project.user.User;
+import com.insta.project.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class MainController {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
     @GetMapping("/login")
-//    @ResponseBody
-    public String loginForm(){
+    public String loginForm() {
         return "login";
     }
 
@@ -17,6 +28,18 @@ public class MainController {
 //    @ResponseBody
     public String signForm(){
         return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signForm(User user) {
+        System.out.println("user");
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/login";
     }
 
     @GetMapping("/story")
